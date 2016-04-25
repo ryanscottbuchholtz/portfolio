eb1b3e8e323605afb578b3032a70b237090a7aa5
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-04-25 12:19:17 -0700

    background image scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+  
+  $(window).scroll( function() {
+    var windowTop = $(window).scrollTop();
+    $('#landing-image').css('background-position', '0px ' + (-windowTop/6) + 'px');
+    console.log(windowTop);
+  });
 })
