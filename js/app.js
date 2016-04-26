9508ce634408ed4e7891f1e08adf1a13b48533ab
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-04-26 12:51:43 -0700

    complete for v2

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
 var landingImageTop = 0;
+var journeyDiv = 0;
+
 
 $(document).ready(function(){
   landingImageTop = parseInt($('#landing-image').css('top'));
   bioPicLeft = parseInt($('#photo-ryan').css('left'));
+  journeyDiv = $('#work').offset().top; 
   
   $(window).scroll( function() {
     var windowTop = $(window).scrollTop();
     var scrollTop = landingImageTop + (windowTop/8);
     $('#landing-image').css('top', scrollTop + 'px');
 
+    var topPosition = (journeyDiv - windowTop - 75);
+    var bottomPosition = (journeyDiv - windowTop - 75 - $('#paths').height());
+
+    if(topPosition < 0 ) {
+      $('#paths').css({
+        opacity: ($('#paths').height() - (75 - (journeyDiv - windowTop)))/$('#paths').height()
+      });
+    }
+
+    console.log((journeyDiv - windowTop) + ' ' + $('#paths').height() + ' ' + (75 - (journeyDiv - windowTop)) + ' ' + ($('#paths').height() - (75 - (journeyDiv - windowTop))) + ' ' + ($('#paths').height() - (75 - (journeyDiv - windowTop))/$('#paths').height()));
 
   });
 })
