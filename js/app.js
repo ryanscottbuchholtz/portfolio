b1ea7c3ab14f33006632f07003783b3d010a95df
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-04-26 14:56:19 -0700

    scroll opacity working

--- js/app.js
+++ js/app.js
@@ -4,24 +4,26 @@ var journeyDiv = 0;
 
 $(document).ready(function(){
   landingImageTop = parseInt($('#landing-image').css('top'));
-  bioPicLeft = parseInt($('#photo-ryan').css('left'));
-  journeyDiv = $('#work').offset().top; 
+  journeyDiv = $('#journey').offset().top; 
   
   $(window).scroll( function() {
     var windowTop = $(window).scrollTop();
     var scrollTop = landingImageTop + (windowTop/8);
-    $('#landing-image').css('top', scrollTop + 'px');
+    if(scrollTop < 75) {
+      $('#landing-image').css('top', scrollTop + 'px');   //max of 75px
+    } else {
+      $('#landing-image').css('top', 75 + 'px');
+    };
 
     var topPosition = (journeyDiv - windowTop - 75);
     var bottomPosition = (journeyDiv - windowTop - 75 - $('#paths').height());
 
     if(topPosition < 0 ) {
-      $('#paths').css({
+      $('#journey').css({
         opacity: ($('#paths').height() - (75 - (journeyDiv - windowTop)))/$('#paths').height()
       });
     }
 
-    console.log((journeyDiv - windowTop) + ' ' + $('#paths').height() + ' ' + (75 - (journeyDiv - windowTop)) + ' ' + ($('#paths').height() - (75 - (journeyDiv - windowTop))) + ' ' + ($('#paths').height() - (75 - (journeyDiv - windowTop))/$('#paths').height()));
-
+    console.log(scrollTop + ' ' + windowTop);
   });
 })
